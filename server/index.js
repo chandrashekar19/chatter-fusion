f371d2ba4d5309012a88055c0249d9bebf238ef7
KalalChandu <kalalshannu19@gmail.com>
2024-10-02 16:20:58 +0530

    Implemented: socket connection with backend

--- server/index.js
+++ server/index.js
@@ -2,4 +2,24 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const PORT = process.env.PORT || 5000;
+
+const router = require("./routes");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("we have a new connection!!!");
+
+  socket.on("disconnect", () => {
+    console.log("user had left!!!");
+  });
+});
+
+app.use(router);
+
+server.listen(PORT, () =>
+  console.log(`Server has started on the port ${PORT}`)
+);
