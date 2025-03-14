3e3d2bcb5512055053336b79dcf5f1f9a7bc8d7e
KalalChandu <kalalshannu19@gmail.com>
2025-03-14 14:51:00 +0530

    changed: the endpoint in the chat.js file

--- client/src/components/chat.jsx
+++ client/src/components/chat.jsx
@@ -22,7 +22,7 @@ const Chat = () => {
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    socket = io(ENDPOINT);
+    socket = io(ENDPOINT, { transports: ["websocket", "polling"] });
 
     setRoom(room);
     setName(name);
